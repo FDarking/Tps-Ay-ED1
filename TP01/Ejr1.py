395d210c0569b46df312f28d1ccdedbf4fa0bd18
FDarking <Fidoyaga@uade.edu.ar>
2024-09-10 16:52:13 -0300

    Funciones

--- TP01/Ejr1.py
+++ TP01/Ejr1.py
@@ -1,1 +1,30 @@
-#Testeo
+def num_mayorstr(a, b, c):
+    if a > b:
+        if a > c:
+            return a
+        elif a < c:
+            return c
+    elif b > a:
+        if b > c:
+            return b
+        elif b < c:
+            return c
+    elif c > a:
+        if c > b:
+            return c
+        elif c < b:
+            return b
+    return -1
+
+
+def valores():
+    a = int(input("Ingrese el primer valor POSITIVO: "))
+    b = int(input("Ingrese el segundo valor POSITIVO: "))
+    c = int(input("Ingrese el tercer valor POSITIVO: "))
+    mayor = num_mayorstr(a, b, c)
+    if mayor != -1:
+        print(f"El número mayor es {mayor}")
+    else:
+        print("No existe un número mayor estricto.")
+
+valores()
---
+++ TP01/ejer4.py
@@ -0,0 +1,15 @@
+def num_mayorestricto ():
+    if a > b :
+        if a < c :
+            return a
+        elif a < c :
+            return c
+    elif b > a:
+        if b < c :
+
+
+
+    
+        
+
+
