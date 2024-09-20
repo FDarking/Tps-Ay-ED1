2588479d6d7a5f6e143c8c87be7c48c9219e8992
FDarking <Fidoyaga@uade.edu.ar>
2024-09-20 02:00:47 -0300

    Dato

---
+++ TP01/ejr6.py
@@ -0,0 +1,14 @@
+"""
+Desarrollar una función que reciba como parámetros dos números enteros positivos
+y devuelva como valor de retorno el número que resulte de concatenar ambos
+parámetros. Por ejemplo, si recibe 1234 y 567 debe devolver 1234567. No se permite utilizar 
+facilidades de Python no vistas en clase.
+"""
+
+
+def concatenar(a: int, b: int):
+    # Concatena dos números enteros cambiandolos a string y luego los retorna como int.
+    c = str(a) + str(b)
+    return int(c)
+
+print(concatenar(542,653))
